b64d107261321669dd32bf83634ac5a00f3a78cd
MukendiMputu <mputusam@live.com>
2021-03-23 07:40:00 +0100

    Stage 4 - Chapter 4 Errors

--- Problems/Cat cafés/main.py
+++ Problems/Cat cafés/main.py
@@ -1,11 +1,12 @@
-usr_input = input()
 cafes = []
 no_cats = []
 while True:
-    if usr_input. "MEOW":
+    usr_input = input()
+    if usr_input == "MEOW":
         break
     cafe, cat = usr_input.split()
     cafes.append(cafe)
     no_cats.append(int(cat))
-print(cafes)
-print(no_cats)
+max_element = max(no_cats)
+idx_max = no_cats.index(max_element)
+print(cafes[idx_max])
